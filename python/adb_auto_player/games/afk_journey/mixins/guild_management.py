96266680cb9903a4da2736990f3939bc57f1b4a9
Wong Jun Zhi <wongjz2002@gmail.com>
2025-08-08 07:40:52 +0800

    Guild functions update

--- python/adb_auto_player/games/afk_journey/mixins/guild_management.py
+++ python/adb_auto_player/games/afk_journey/mixins/guild_management.py
@@ -8,7 +8,14 @@ from adb_auto_player.decorators import register_command, register_custom_routine
 from adb_auto_player.exceptions import (
     GameTimeoutError,
 )
+from adb_auto_player.image_manipulation import (
+    Cropping,
+)
+from adb_auto_player.models import ConfidenceValue
 from adb_auto_player.models.decorators import GUIMetadata
+from adb_auto_player.models.geometry import Point
+from adb_auto_player.models.image_manipulation import CropRegions
+from adb_auto_player.template_matching import TemplateMatcher
 
 from ..base import AFKJourneyBase
 from ..gui_category import AFKJCategory
@@ -59,14 +66,35 @@ class GuildManagement(AFKJourneyBase):
                 logging.error(f"{e} {self.LANG_ERROR}")
                 return None
 
-        while not self.game_find_template_match(
-            template="guild/guild_member_list_bottom.png"
-        ):
-            self.swipe_up(sy=1350, ey=379, duration=15)
+        attempts = 0
+        max_attempts = 10
+        old_screenshot = Cropping.crop(
+            self.get_screenshot(), crop_regions=CropRegions(top="50%")
+        )
+        while True:
+            self.swipe_up(sy=1350, ey=378, duration=15)
+            self.tap(Point(x=1070, y=960), scale=True)
             sleep(2)
 
-        # Implement guild management logic here
-        # For example, checking guild status, performing actions, etc.
+            new_screenshot = Cropping.crop(
+                self.get_screenshot(), crop_regions=CropRegions(top="50%")
+            )
+
+            if TemplateMatcher.similar_image(
+                base_image=new_screenshot.image,
+                template_image=old_screenshot.image,
+                threshold=ConfidenceValue("95%"),
+            ):
+                logging.info("Reached the end of the member list.")
+                break
+
+            old_screenshot = new_screenshot
+            attempts += 1
+            if attempts >= max_attempts:
+                logging.warning(
+                    "Reached maximum attempts to scroll through the member list."
+                )
+                break
 
     def _is_on_guild_member_list(self) -> bool:
         """Check if the current screen is the Guild Management screen."""
@@ -82,6 +110,24 @@ class GuildManagement(AFKJourneyBase):
         return (
             self.game_find_template_match(
                 template="guild/clashfronts_team_selection.png",
+            )
+            is not None
+        )
+
+    def _is_in_dream_realm_rank(self) -> bool:
+        """Check if the current screen is in Dream Realm Rank."""
+        return (
+            self.game_find_template_match(
+                template="guild/dr_rank.png",
+            )
+            is not None
+        )
+
+    def _is_dream_realm_rank_filtered(self) -> bool:
+        """Check if the Dream Realm Rank is filtered."""
+        return (
+            self.game_find_template_match(
+                template="guild/dr_member_filter.png",
             )
             is not None
         )
